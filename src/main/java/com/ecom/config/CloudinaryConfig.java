217cdfc9e9fac01f11f8d8927514e4f610aa1e35
cudeoid <nihalnandannayak123@gmail.com>
2024-11-25 20:19:59 +0530

    add project

---
+++ src/main/java/com/ecom/config/CloudinaryConfig.java
@@ -0,0 +1,63 @@
+
+package com.ecom.config;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+//import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
+    }
+}
+
+
+
+
+
+
+//import org.springframework.beans.factory.annotation.Value;
+
+//@Configuration
+//public class CloudinaryConfig {
+//
+//    @Value("${cloudinary.cloud-name}")
+//    private String cloudName;
+//
+//    @Value("${cloudinary.api-key}")
+//    private String apiKey;
+//
+//    @Value("${cloudinary.api-secret}")
+//    private String apiSecret;
+//
+//    @Bean
+//    public Cloudinary cloudinary() {
+//        Map<String, String> config = new HashMap<>();
+//        config.put("cloud_name", cloudName);
+//        config.put("api_key", apiKey);
+//        config.put("api_secret", apiSecret);
+//        return new Cloudinary(config);
+//    }
+//}
---
+++ src/main/java/com/ecom/service/impl/CloudinaryService.java
@@ -0,0 +1,28 @@
+package com.ecom.service.impl;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    @Autowired
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        return uploadResult.get("url").toString();  // Returns the URL of the uploaded image
+    }
+
+
+}
